477eae5db4e8560ad58205826049d101d5ab9b8a
Gustavo Alves <gustavoalvess842@gmail.com>
2026-01-21 12:36:28 -0300

    Atualizações do projeto de emprestimo

--- exercicio36.py
+++ exercicio36.py
@@ -1,3 +1,7 @@
+# Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. 
+# Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar. 
+# A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado.
+
 largura = 40
 
 print("=-=".center(largura, "="))
@@ -7,3 +11,14 @@ print("=-=".center(largura, "="))
 casa = float(input("Qual o valor da casa? R$"))
 salario = float(input("Sálario do comprador: R$"))
 tempo = int(input("Quantos anos de financiamento? "))
+
+prestacao = casa / (tempo * 12)
+minimo = salario * 30 / 100
+
+print(f"Para pagar uma casa de {casa} de R${casa:.2f} em {tempo} anos")
+print(f"a prestação será de R${prestacao:.2f}")
+
+if prestacao <= minimo:
+    print("Emprestimo pode ser CONCEDIDO!")
+else:
+    print("\033[0;41mEmprestimo NEGADO\033[m!")
