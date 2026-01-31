3313e40d20a592c7450339839564d8b47704bb82
Gustavo Alves <gustavoalvess842@gmail.com>
2026-01-31 10:06:05 -0300

    Add atualizações

--- exercicio44.py
+++ exercicio44.py
@@ -11,8 +11,10 @@ FORMAS DE PAGAMENTO
 [ 4 ] 3x ou mais no cartão
 ''')
 opcao = int(input("Qual é a opção? "))
-avista10 = (compras * 10) /100
-avista5 = (compras * 5) /100
+avista10 = (compras * 10) / 100
+avista5 = (compras * 5) / 100
+parcelado = (compras * 20) / 100 
+compras = parcelado + compras
 
 if opcao == 1:
     print(f"Sua compra de R${compras:.2f} vai custar R${compras - avista10:.2f} no final.")
@@ -20,3 +22,8 @@ elif opcao == 2:
     print(f"Sua compra de R${compras:.2f} vai custar R${compras - avista5:.2f} no final.")
 elif opcao == 3:
     print(f"Sua compra de R${compras} ficou parcelado 2x em {compras / 2}")
+elif opcao == 4:
+    vezes = int(input("Em quantas vezes você vai pagar?: "))
+    if vezes == 2:
+        print(opcao)
+    print(f"Sua compra de R${compras:.2f} parcelada em {vezes}x ficou em R${compras / vezes}")
