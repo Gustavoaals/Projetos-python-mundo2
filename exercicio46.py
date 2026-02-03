777baeac6c283f930d258b70acaaff9e484e8f59
Gustavo Alves <gustavoalvess842@gmail.com>
2026-02-03 20:30:59 -0300

    Add Att no 46 e 47

--- exercicio46.py
+++ exercicio46.py
@@ -2,4 +2,4 @@ import time
 for cont in range(10, -1, -1):
     print(cont)
     time.sleep(1)
-print("Feliz ANO NOVOO!")
+print("BUMMMM!")
--- exercicio47.py
+++ exercicio47.py
@@ -1,3 +1,3 @@
-for n in range(1,50):
-    pares = n % 2
-print(f"Os números de entre 1 e 50 são {pares}")
+for n in range(2, 51, 2):
+    print(n, end= ' ')
+print("\nAcacbou")
