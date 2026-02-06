fb4bf274d41ddd2fe5e63dae484b50c6d68daa86
Gustavo Alves <gustavoalvess842@gmail.com>
2026-02-06 13:03:46 -0300

    Att exercicio 48

--- exercicio48.py
+++ exercicio48.py
@@ -7,4 +7,4 @@ for c in range(1, 501, 2):
     if c % 3 == 0:
         cont += 1 # Contador
         soma = soma + c # Somador
-print(f"A soma de todos os {cont}valores solicitados é {soma}")
+print(f"A soma de todos os {cont} valores solicitados é {soma}")
