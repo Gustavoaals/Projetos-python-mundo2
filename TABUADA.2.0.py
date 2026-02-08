e74381ba1409bcb8b06414065a58e007e1f77963
Gustavo Alves <gustavoalvess842@gmail.com>
2026-02-08 14:51:49 -0300

    Add tabuada 2.0

--- TABUADA.2.0.py
+++ TABUADA.2.0.py
@@ -2,15 +2,26 @@ print('=-' * 15)
 print("TABUADA".center(30))
 print('=-' * 15)
 
-print('''\nQual operação Matematica desejada?\n
+tabu = int(input("TABUADA: "))
+
+print("""
 [ 1 ] Adição
 [ 2 ] Subtração
 [ 3 ] Multiplicação
-[ 4 ] Divisão\n ''')
+[ 4 ] Divisão
+""")
 
-num = int(input("Digite um número: "))
-tabu = int(input("Tabuada: "))
+num = int(input("NÚMERO: "))
 
-for n in range(0,11):
-    if num == 1:
-        print(f"{tabu} + {n} = {tabu + n} ") 
+if num < 1 or num > 4:
+    print("Número inválido.")
+else:
+    for n in range(1, 11):
+        if num == 1:
+            print(f"{tabu} + {n} = {tabu + n}")
+        elif num == 2:
+            print(f"{tabu} - {n} = {tabu - n}")
+        elif num == 3:
+            print(f"{tabu} x {n} = {tabu * n}")
+        elif num == 4:
+            print(f"{tabu * n} ÷ {tabu} = {(tabu * n) / tabu}")
