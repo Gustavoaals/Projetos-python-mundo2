7cb7d96fd4db687764fb067e71fcfc013f3a65fc
Gustavo Alves <gustavoalvess842@gmail.com>
2026-02-18 12:10:24 -0300

    Exercicio55

---
+++ exercicio55.py
@@ -0,0 +1,15 @@
+#Faça um programa que leia o peso de cinco pessoas.
+#No final, mostre qual foi o maior e o menor peso lidos.
+
+for pessoa in range(1,6):
+    peso = float(input(f"Digite o peso da {pessoa}ª pessoa: "))
+    if pessoa == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso 
+        if peso < menor:
+            menor = peso
+print(f"O maior peso lido foi {maior}Kg")
+print(f"O menor peso lido foi {menor}Kg")
